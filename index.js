309e0134f7bb72424b6b38e173c5ea20846ec3f1
Zahira Khan <66169164+zahirakhan@users.noreply.github.com>
2024-03-23 09:44:10 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: chalk.blue("Please Guess a Number between 1 to 6:"),
+    },
+]);
+if (answer.userGuessNumber === randomNumber) {
+    console.log(chalk.green("Congratulations! You Guess Right Number"));
+}
+else {
+    console.log(chalk.red("Sorry! You Guess Wrong Number"));
+}
